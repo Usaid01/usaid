8a4a3b661d214112522e759652cb6669f5b9808f
Usaid01 <71736607+Usaid01@users.noreply.github.com>
2021-06-06 15:18:07 -0400

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,26 @@
+package Card;
+
+
+/**
+ *This class +++Insert Description here+++
+ *
+ * @author Usaid Zahid
+ */
+public class Card {
+public static void main(String[] args) {
+        
+        Card[] magicHand = new Card[7];
+
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();
+            //c.setValue(insert call to random number generator here)
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        }
+
+        //insert code to ask the user for Card value and suit, create their card
+        // and search magicHand here
+        //Then report the result here
+    }
+
+}
+
